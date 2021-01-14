a8b9321363ea115fc43f97d60a0ccadccbb4259c
SoySause115 <jasonkozin115@gmail.com>
2021-01-14 14:20:06 -0500

    changes from week 1 thursday class

---
+++ week1/staticServer/app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var port = process.env.PORT || 3000
+
+//build a route for the index page
+app.get('/', function(request, response){
+    response.sendFile(path.join(__dirname + '/views/index.html'))
+})
+
+app.use(express.static(__dirname + '/views'))
+
+app.listen(port, function(){
+    console.log("Connected to Port 3000")
+})
+
